4430fec57d89ecb80a50a713882fba9ee9ea840a
seed-master <akira75@gmx.net>
2022-06-14 23:12:16 +0200

    EUU-79

--- src/main/java/org/seed/core/form/FormServiceImpl.java
+++ src/main/java/org/seed/core/form/FormServiceImpl.java
@@ -666,12 +666,21 @@ public class FormServiceImpl extends AbstractApplicationEntityService<Form>
 		Assert.notNull(session, C.SESSION);
 		
 		for (Form form : formRepository.find(session, queryParam(C.ENTITY, entity))) {
+			// delete sub form if nested no longer exist
 			if (form.hasSubForms()) {
-				// delete sub form if nested no longer exist
 				for (SubForm subForm : new ArrayList<>(form.getSubForms())) {
 					if (!entity.containsNested(subForm.getNestedEntity())) {
 						form.removeSubForm(subForm);
 						session.delete(subForm);
+					}
+				}
+			}
+			// delete field extra if entity field no longer exist
+			if (form.hasFieldExtras()) {
+				for (FormFieldExtra fieldExtra : new ArrayList<>(form.getFieldExtras())) {
+					if (!entity.containsField(fieldExtra.getEntityField())) {
+						form.removeFieldExtra(fieldExtra);
+						session.delete(fieldExtra);
 					}
 				}
 			}
